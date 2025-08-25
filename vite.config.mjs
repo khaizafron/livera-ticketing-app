f2358eb7a7f2d346e9cdf02f68f72de8fb86af49
khaizafron <khaizafron@gmail.com>
2025-08-25 10:24:14 +0800

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,8 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [react()],
+  base: "/livera-ticketing-app/",
   // This changes the output dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
